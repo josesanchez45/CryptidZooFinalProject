fdb22a8ceaa6d025c48b8111da59b8f9826e7156
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-21 16:29:09 -0700

    initial commit

---
+++ CryptidZooProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptidZooProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
