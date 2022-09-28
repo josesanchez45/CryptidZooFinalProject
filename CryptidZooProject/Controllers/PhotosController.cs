d7b8201afa0af65ab91f6be6ebb7846e0e2a2425
jose sanchez <jose.sanchez.artist@gmail.com>
2022-09-28 09:34:54 -0700

    added images

---
+++ CryptidZooProject/Controllers/PhotosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CryptidZooProject.Controllers
+{
+    public class PhotosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
